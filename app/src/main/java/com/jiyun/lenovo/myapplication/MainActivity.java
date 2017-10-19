754928f912f44be07e97f05dc3f19307fba43cb9
liurongb <1270969067@qq.com>
2017-10-19 10:58:52 +0800

    代码提交two

--- app/src/main/java/com/jiyun/lenovo/myapplication/MainActivity.java
+++ app/src/main/java/com/jiyun/lenovo/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (BuildConfig.DEBUG) Log.d("", "");
+        if (BuildConfig.DEBUG) Log.d("我爱Java", "Java爱我");
     }
 }
